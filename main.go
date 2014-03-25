287155c79802fd817c5324a283d5fdba53f0a73e
Ryan Uber <ru@ryanuber.com>
2014-03-25 00:11:44 -0700

    pharos: initial commit

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ryanuber/pharos/pharos"
+)
+
+func main() {
+	fmt.Printf("pharos version %s\n", pharos.Version)
+}
---
+++ pharos/version.go
@@ -0,0 +1,3 @@
+package pharos
+
+const Version = "0.0.1"
