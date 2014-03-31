da176e27417fe089f69058cfab5134fa030a2e09
Ryan Uber <ru@ryanuber.com>
2014-03-30 19:47:25 -0700

    Added new upstreamer method and made name/addr public. Added upstream interval.

--- pharos/upstream.go
+++ pharos/upstream.go
@@ -1,6 +1,25 @@
 package pharos
 
+import (
+	"time"
+)
+
 type Upstream struct {
-	upstreamName    string
-	upstreamAddress string
+	Name string
+	Addr string
+
+	// Determines how often data is upstreamed
+	Interval time.Duration
+}
+
+func NewUpstream(name string, addr string) *Upstream {
+	return &Upstream{
+		Name: name,
+		Addr: addr,
+	}
+}
+
+// Send collected data to an upstream source
+func (u *Upstream) Submit() error {
+	return nil
 }
