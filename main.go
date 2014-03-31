b642e87e32c3da49dd89b747d727eff64939d2f4
Ryan Uber <ru@ryanuber.com>
2014-03-30 20:06:46 -0700

    Added a second test to main with different timing values

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 
 func main() {
 	fmt.Printf("pharos version %s\n", pharos.Version)
-	process := monitor.NewMonitoredCommand("true", 1)
-	process.Monitor()
+
+	fmt.Println("starting to monitor 'true' (1s)")
+	go monitor.NewMonitoredCommand("true", 1).Monitor()
+
+	fmt.Println("starting to monitor 'false' (3s)")
+	monitor.NewMonitoredCommand("false", 3).Monitor()
 }
