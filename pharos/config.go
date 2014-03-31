496c5074d05183c217593eb9ae4d1cdc7c194811
Ryan Uber <ru@ryanuber.com>
2014-03-30 19:34:18 -0700

    Added config skeleton

---
+++ pharos/config.go
@@ -0,0 +1,25 @@
+package pharos
+
+type Config struct {
+	// Address and port to bind Pharos to
+	BindAddr string
+	BindPort uint16
+
+	// Address and port to bind the REST interface to
+	EnableHTTP bool
+	HTTPAddr string
+	HTTPPort uint16
+
+	// Event upstreaming settings
+	EnableUpstreams bool
+	Upstreams       []*Upstream
+}
+
+func DefaultConfig *Config {
+	return &Config{
+		BindAddr: "0.0.0.0",
+		BindPort: 22322,
+		EnableHTTP: false,
+		EnableUpstreams: true,
+	}
+}
